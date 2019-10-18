47ff04e54e58643d417d6da9314564895ccfeded
jeanshotjack <dshalenkova@gmail.com>
2019-10-18 17:37:35 -0400

    created variables and math function for computer guess, ran console log

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var win = 0;
+var loss = 0;
+var lives = 8;
+var guess;
+
+var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var computer = letter[Math.floor(Math.random() * letter.length)];
+console.log(computer);
