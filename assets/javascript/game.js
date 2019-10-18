38bf8603cbf6b19b5ad89e943f36041319d4ef16
jeanshotjack <dshalenkova@gmail.com>
2019-10-18 17:50:05 -0400

    added script to make counter go to the DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,16 @@
 var win = 0;
 var loss = 0;
 var lives = 8;
-var guess;
+var guess = [];
 
 var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var computer = letter[Math.floor(Math.random() * letter.length)];
 console.log(computer);
+
+// ------------------------------
+
+document.getElementById("win").innerHTML = + win;
+document.getElementById("loss").innerHTML = + loss;
+document.getElementById("lives").innerHTML = + lives;
+document.getElementById("guess").innerHTML = + guess;
