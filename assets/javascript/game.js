cadba8911ed985f9010d7373300d167deede00a7
jeanshotjack <dshalenkova@gmail.com>
2019-10-18 20:57:47 -0400

    gonna throw my computer then myself out the fucking window

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,30 +12,34 @@ console.log(computer);
 // Need to get it to read the key person presses
 document.onkeypress = function (event) {
     user = event.key;
-   
-    for (lives = 0; lives < 8; lives++) {
-    // Need to test guesses then add win/loss points
-    if (user === computer) {
-        win++;
-        document.getElementById("win").innerHTML = "Wins: " + win;
-    }
 
-    else if (user !== computer) {
-        lives--;
-        document.getElementById("lives").innerHTML = "Lives left: " + lives;
-        var wrong = guess.push(event.key);
-        document.getElementById("guess").innerHTML = "Letters you have guessed: " + guess;
-        wrong.textContent = guess.join("");
-    }
-    }
-    if (lives === 0) {
+    function(justPleaseWork) { //THE FUFCK DO YOU PUT HERE TO MAKE IT WORK
+        // Need to test guesses then add win/loss points
+        if (user === computer) {
+            win++;
+            document.getElementById("win").innerHTML = "Wins: " + win;
+            computer = letter[Math.floor(Math.random() * letter.length)];
+            console.log(computer);
+        }
+
+        else if (user !== computer) {
+            lives--;
+            document.getElementById("lives").innerHTML = "Lives left: " + lives;
+            var wrong = guess.push(event.key);
+            document.getElementById("guess").innerHTML = "Letters you have guessed: " + guess;
+            wrong.textContent = guess.join("");
+        }
+}
+        if (lives === 0) {
         loss++;
         document.getElementById("loss").innerHTML = "Losses: " + loss;
-        var computer = letter[Math.floor(Math.random() * letter.length)];
+        computer = letter[Math.floor(Math.random() * letter.length)];
         console.log(computer);
     }
-    
+
 }
+
+// bRO WHY DIESNT THIS SHIT WORKN 
 
 
 
